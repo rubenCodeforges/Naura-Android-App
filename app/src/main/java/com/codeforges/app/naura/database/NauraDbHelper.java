e6e75181bb557be4ba2c7524a99252fd890ce507
rubenCodeforges <ruben@codeforges.com>
2015-04-25 13:10:21 +0300

    close #13

--- app/src/main/java/com/codeforges/app/naura/database/NauraDbHelper.java
+++ app/src/main/java/com/codeforges/app/naura/database/NauraDbHelper.java
@@ -9,6 +9,7 @@ import android.util.Log;
 
 import com.codeforges.app.naura.models.EntityInterface;
 import com.codeforges.app.naura.models.NauraData;
+import com.codeforges.app.naura.models.User;
 
 /**
  * Created by Codeforges on 18.04.15.
@@ -16,18 +17,12 @@ import com.codeforges.app.naura.models.NauraData;
 
 public class NauraDbHelper extends SQLiteOpenHelper {
 
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "naura.db";
     private static final String TABLE_DATA = "naura_data";
-    private static final String COLUMN_NAMES =
-              "_id,"
-            + "item_title,"
-            + "item_data,"
-            + "item_images,"
-            + "uploaded";
-
+    private static final String TABLE_USER = "naura_user";
     // Database creation sql statement
-    private static final String DATABASE_CREATE = "CREATE TABLE "+TABLE_DATA+" ("
+    private static final String DATABASE_CREATE_DATA = "CREATE TABLE " + TABLE_DATA + " ("
             + "	_id integer primary key autoincrement,"
             + "item_title TEXT ,"
             + "item_data TEXT ,"
@@ -35,13 +30,21 @@ public class NauraDbHelper extends SQLiteOpenHelper {
             + "uploaded integer"
             + ")";
 
-    public NauraDbHelper ( Context context ) {
-        super (context, DATABASE_NAME, null, DATABASE_VERSION);
+    private static final String DATABASE_CREATE_USER = "CREATE TABLE " + TABLE_USER + " ("
+            + "	_id integer primary key autoincrement,"
+            + "name TEXT ,"
+            + "special_id TEXT ,"
+            + ")";
+
+
+    public NauraDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL(DATABASE_CREATE);
+        sqLiteDatabase.execSQL(DATABASE_CREATE_DATA);
+        sqLiteDatabase.execSQL(DATABASE_CREATE_USER);
     }
 
     @Override
@@ -50,31 +53,37 @@ public class NauraDbHelper extends SQLiteOpenHelper {
                 "Upgrading database from version " + oldVersion + " to "
                         + newVersion + ", which will destroy all old data");
         sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_DATA);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_USER);
         onCreate(sqLiteDatabase);
     }
 
     public void addProduct(EntityInterface data) {
         ContentValues values = new ContentValues();
-        if(data instanceof NauraData) {
+        if (data instanceof NauraData) {
             values.put("item_title", ((NauraData) data).getItemTitle());
             values.put("item_data", ((NauraData) data).getItemData());
             values.put("item_images", ((NauraData) data).getItemImages());
             values.put("uploaded", ((NauraData) data).getUploaded());
         }
-        flushDb(values);
+        if (data instanceof User) {
+            values.put("name", ((User) data).getName());
+            values.put("special_id", ((User) data).getSpecialId());
+        }
+        flushDb(values,data.getClass().toString());
     }
 
-    // Getting All Contacts
-    // TODO : Issue #13
-    public Cursor getAllData() {
-        String selectQuery = "SELECT  * FROM " + TABLE_DATA;
-        Cursor cursor = this.getWritableDatabase().rawQuery(selectQuery,null);
+
+
+    public Cursor findAll(String table) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String selectQuery = "SELECT  * FROM " + (table == ("user") ? TABLE_USER : TABLE_DATA);
+        Cursor cursor = db.rawQuery(selectQuery, null);
         return cursor;
     }
 
-    private void flushDb (ContentValues values) {
+    private void flushDb(ContentValues values, String table) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.insert(TABLE_DATA, null, values);
+        db.insert(table.equals(User.class.toString()) ? TABLE_USER : TABLE_DATA, null, values);
         db.close();
     }
 }
